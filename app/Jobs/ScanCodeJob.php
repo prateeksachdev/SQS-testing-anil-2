78851f316633431eaf721ab9322ee9897082df9b
Anil Thakur <anilthakur@mobikasa.com>
2023-03-01 10:26:45 +0530

    multiplejobs

--- app/Jobs/ScanCodeJob.php
+++ app/Jobs/ScanCodeJob.php
@@ -43,7 +43,7 @@ class ScanCodeJob implements ShouldQueue
         // DB::table('users_status')->where('user_id', $UIds)->update(['process_start_at'=>now()]);
 
         // $codes = DB::table('users_status')->where('user_id',$UIds)->pluck('scan_code')->toArray();
-        $codes = DB::table('users_status')->where('scan_code',$UIds)->pluck('scan_code')->toArray();
+        $codes = DB::table('users_status')->where('scan_code',$this->details['scan_code'])->pluck('scan_code')->toArray();
         foreach($codes as $sCode){
 
      
